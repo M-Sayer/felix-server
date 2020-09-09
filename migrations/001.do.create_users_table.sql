8568efa5a935a3cddbd75b03fb9f3e960ba5b04d
Gage <gagemigan@gmail.com>
2020-09-09 11:01:13 -0400

    merge fixes

--- migrations/001.do.create_users_table.sql
+++ migrations/001.do.create_users_table.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS "users" (
     "password" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "allowance" NUMERIC(12,2) CHECK( allowance >= 0) DEFAULT 0 NOT NULL ,
-    "balance" NUMERIC(12,2) CHECK( balance >= 0) DEFAULT 0 NOT NULL 
+    "balance" NUMERIC(12,2) DEFAULT 0 NOT NULL 
 );
