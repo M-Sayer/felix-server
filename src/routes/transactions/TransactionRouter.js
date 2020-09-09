0186ed8e796271d383a8d5b53dd8ecc776eefd98
Gage <gagemigan@gmail.com>
2020-09-09 12:38:06 -0400

    added name columns

--- src/routes/transactions/TransactionRouter.js
+++ src/routes/transactions/TransactionRouter.js
@@ -1,9 +1,55 @@
 const express = require('express');
 
 const TransactionRouter = express.Router();
+
 const TransactionServices = require('./TransactionServices');
+
+/**
+ * expects something like [
+ *         {
+ *          income:{
+ *               val : 123456,
+ *               type: 'foo',
+ *              } 
+ *          }, 
+ *          {
+ *          income:{
+ *               val : 123456,
+ *               type: 'foo',
+ *              } 
+ *          }, 
+ *          {
+ *          expense:{
+ *               val : 123456,
+ *               type: 'foo',
+ *              } 
+ *          },
+ *           {
+ *          income:{
+ *               val : 123456,
+ *               type: 'foo',
+ *              } 
+ *          },
+ *           {
+ *          expense:{
+ *               val : 123456,
+ *               type: 'foo',
+ *              } 
+ *          },
+ *           {
+ *          expense:{
+ *               val : 123456,
+ *               type: 'foo',
+ *              } 
+ * }....]
+ */
 
 TransactionRouter
 .get('/',(req,res,next) =>{
-    
-})
+
+});
+
+
+
+
+module.exports = TransactionRouter;
