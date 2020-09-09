8594ff0189269f83ff352cb4334d3f55973e22b6
Gage <gagemigan@gmail.com>
2020-09-09 11:40:07 -0400

    added config info and knex to server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
-const app = require("./app");
+const app = require('./app');
+const knex = require('knex');
+const {PORT , DATABASE_URL} = require('./config');
+
+const db = knex({
+  client : 'pg',
+  connection : DATABASE_URL,
+});
 
 // our localhost dev server will listen on port 8000
 app.listen(8000, () => {
-  console.log('Felix is assisting on port 8000');
+  console.log(`Felix is assisting on port ${PORT}`);
 });
