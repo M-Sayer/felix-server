ae5dade63252365ccfde0a0b72fb9b119b3ca098
mischkiey <mischkieycf@gmail.com>
2020-09-14 10:04:36 -0400

    Changed single to double quotes in migrations

--- migrations/002.do.create_goals_table.sql
+++ migrations/002.do.create_goals_table.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS 'goals' (
-  'id' SERIAL PRIMARY KEY,
-  'user_id' INTEGER REFERENCES 'users'(id) 
+CREATE TABLE IF NOT EXISTS "goals" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" INTEGER REFERENCES "users"(id) 
     ON DELETE CASCADE NOT NULL,
-  'goal_amount' NUMERIC(12,2) CHECK (goal_amount > 0) 
+  "goal_amount" NUMERIC(12,2) CHECK (goal_amount > 0) 
     DEFAULT 0 NOT NULL,
-  'contribution_amount' NUMERIC(12,2) CHECK (contribution_amount > 0)
+  "contribution_amount" NUMERIC(12,2) CHECK (contribution_amount > 0)
     DEFAULT 0 NOT NULL,
-  'current_amount' NUMERIC(12,2) CHECK (current_amount >= 0)
+  "current_amount" NUMERIC(12,2) CHECK (current_amount >= 0)
     DEFAULT 0 NOT NULL,
-  'end_date' DATE DEFAULT now() NOT NULL,
-  'completed' BOOLEAN DEFAULT false NOT NULL,
-  'date_created' TIMESTAMPTZ DEFAULT now() NOT NULL
+  "end_date" DATE DEFAULT now() NOT NULL,
+  "completed" BOOLEAN DEFAULT false NOT NULL,
+  "date_created" TIMESTAMPTZ DEFAULT now() NOT NULL
 );
